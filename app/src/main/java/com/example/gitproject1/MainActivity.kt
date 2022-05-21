7ad3c9673b6a37e8d93dc5b65253e1f5c6dfcc18
Cesar <cisneros627@gmail.com>
2022-05-21 16:50:55 -0700

    I added the time to the Main Acticity on start!

--- app/src/main/java/com/example/gitproject1/MainActivity.kt
+++ app/src/main/java/com/example/gitproject1/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.gitproject1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.gitproject1.databinding.ActivityMainBinding
+import java.util.*
+import java.text.SimpleDateFormat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var dateTime: String
+    lateinit var calendar:Calendar
+    lateinit var simpleDateFormat: SimpleDateFormat
+
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        calendar = Calendar.getInstance()
+        simpleDateFormat = SimpleDateFormat("dd/MM/yyyy KK:mm:ss aaa z","Locale.US")
+        dateTime = simpleDateFormat.format(calendar.time).toString()
+
+
+    binding.localTime.text = dateTime
+
     }
+
+
+
+
 }
